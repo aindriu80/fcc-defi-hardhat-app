dbee817527944929123bbb0d45f342bb87e67d33
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-11-04 14:24:50 +0000

    Borrowing from Aave

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -19,7 +19,46 @@ async function main() {
   console.log('Depositing....')
   await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0)
   console.log('deposited...')
+  let { availableBorrowsETH, totalDebtETH } = await getBorrowedUserData(lendingPool, deployer)
+
+  const daiPrice = await getDaiPrice()
+  const amountDaiToBorrow = availableBorrowsETH.toString() * 0.95 * (1 / daiPrice.toNumber())
+  console.log(`You can borrow ${amountDaiToBorrow} DAI`)
+  const amountDaiToBorrowWei = ethers.utils.parseEther(amountDaiToBorrow.toString())
+
+  // availableBorrowsETH ?? What the conversion rate on DAI is?
+  // Borrow Time!
+  // How much have we have borrowed, how much we have in collateral, how much can we borrow
+  const daiToken = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
+  await borrowDai(daiTokenAddress, lendingPool, amountDaiToBorrowWei, deployer)
+  await getBorrowedUserData(lendingPool, deployer)
 }
+
+async function borrowDai(daiAddress, lendingPool, amountDaiToBorrowWei, account) {
+  const borrowTx = await lendingPool.borrow(daiAddress, amountDaiToBorrow, 1, 0, account)
+  await borrowTx.wait(1)
+  console.log(`You have borrowed!`)
+}
+
+async function getBorrowedUserData(lendingPool, account) {
+  const { totalCollateralETH, totalDebtETH, availableBorrowsETH } =
+    await lendingPool.getUserAccountData(account)
+  console.log(`You have ${totalCollateralETH}worth of ETH deposited.`)
+  console.log(`You have ${totalDebtETH} worth of ETH borrowed.`)
+  console.log(`You can borrow ${availableBorrowsETH} worth of ETH.`)
+  return { availableBorrowsETH, totalDebtETH }
+}
+
+async function getDaiPrice() {
+  const daiEthPriceFeed = await ethers.getContractAt(
+    'AggregatorV3Interfaces',
+    '0x773616E4d11A78F511299002da57A0a94577F1f4'
+  )
+  const price = (await daiEthPriceFeed.latestRoundData())[1]
+  console.log(`The DAI/ETH price is {price.toString()}`)
+  return price
+}
+
 
 async function getLendingPool(account) {
   const lendingPoolAddressesProvider = await ethers.getContractAt(
